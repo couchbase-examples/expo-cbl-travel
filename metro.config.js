1dc6f00704bab50c66bf6be590a48eeec5b19d33
Jayant Dhingra <jayant.dhingra@couchbase.com>
2026-06-14 13:52:01 +0530

    refactor: Enhance Metro configuration for cbl-reactnative integration
    - Replace static nodeModulesPaths with a dynamic Proxy for extraNodeModules to streamline module resolution.
    - Add additional blockList entries to prevent conflicts with various cbl-reactnative submodules.

--- metro.config.js
+++ metro.config.js
@@ -5,17 +5,29 @@ const config = getDefaultConfig(__dirname);
 const cblReactNativePath = path.resolve(__dirname, '../cbl-reactnative');
 
 config.watchFolders = [cblReactNativePath];
-config.resolver.nodeModulesPaths = [
-  path.resolve(__dirname, 'node_modules'),
-  path.resolve(cblReactNativePath, 'node_modules'),
-];
-config.resolver.extraNodeModules = {
-  'cbl-reactnative': cblReactNativePath,
-};
+
+config.resolver.extraNodeModules = new Proxy(
+  {
+    'cbl-reactnative': cblReactNativePath,
+  },
+  {
+    get: (target, name) => {
+      if (Object.prototype.hasOwnProperty.call(target, name)) {
+        return target[name];
+      }
+      return path.join(__dirname, 'node_modules', name);
+    },
+  }
+);
+
 config.resolver.unstable_enableSymlinks = true;
 config.resolver.blockList = [
   /.*\/cbl-reactnative\/node_modules\/react-native\/.*/,
   /.*\/cbl-reactnative\/node_modules\/react\/.*/,
+  /.*\/cbl-reactnative\/expo-example\/.*/,
+  /.*\/cbl-reactnative\/couchbase-lite-ios\/.*/,
+  /.*\/cbl-reactnative\/cbl-reactnative-docs\/.*/,
+  /.*\/cbl-reactnative\/ios-swift-quickstart\/.*/,
 ];
 
 const defaultResolveRequest = config.resolver.resolveRequest;
