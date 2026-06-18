3dc2fc6a55934f4ce9ffaade1989fdb15a2cf4e9
Jayant Dhingra <jayant.dhingra@couchbase.com>
2026-06-18 11:35:59 +0530

    chore: consume cbl-reactnative from ProGet npm feed
    Switch from local file dependency to couchbase-lite-react-native@1.1.0-14
    and simplify Metro config for the published package layout.

--- metro.config.js
+++ metro.config.js
@@ -1,56 +1,10 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
 const config = getDefaultConfig(__dirname);
-const cblReactNativePath = path.resolve(__dirname, '../cbl-reactnative');
 
-config.watchFolders = [cblReactNativePath];
-
-config.resolver.extraNodeModules = new Proxy(
-  {
-    'cbl-reactnative': cblReactNativePath,
-  },
-  {
-    get: (target, name) => {
-      if (Object.prototype.hasOwnProperty.call(target, name)) {
-        return target[name];
-      }
-      return path.join(__dirname, 'node_modules', name);
-    },
-  }
-);
-
-config.resolver.unstable_enableSymlinks = true;
 config.resolver.blockList = [
   /.*\/cbl-reactnative\/node_modules\/react-native\/.*/,
   /.*\/cbl-reactnative\/node_modules\/react\/.*/,
-  /.*\/cbl-reactnative\/expo-example\/.*/,
-  /.*\/cbl-reactnative\/couchbase-lite-ios\/.*/,
-  /.*\/cbl-reactnative\/cbl-reactnative-docs\/.*/,
-  /.*\/cbl-reactnative\/ios-swift-quickstart\/.*/,
 ];
-
-const defaultResolveRequest = config.resolver.resolveRequest;
-config.resolver.resolveRequest = (context, moduleName, platform) => {
-  if (
-    /\/lib\/(commonjs|module)\//.test(context.originModulePath) &&
-    moduleName.startsWith('./cblite-js/')
-  ) {
-    const suffix = moduleName
-      .slice('./cblite-js/'.length)
-      .replace(/\.js$/, '');
-    const sourcePath = path.resolve(
-      cblReactNativePath,
-      'cblite-js-common',
-      'cblite-js',
-      suffix
-    );
-    return context.resolveRequest(context, sourcePath, platform);
-  }
-  if (defaultResolveRequest) {
-    return defaultResolveRequest(context, moduleName, platform);
-  }
-  return context.resolveRequest(context, moduleName, platform);
-};
 
 module.exports = config;
